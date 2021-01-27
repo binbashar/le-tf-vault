3bf011b9d4204029f15c39294827058888cac6e0
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2021-01-27 11:40:19 -0300

    BBL-262 | vault/policies -> admins, devops and devs

---
+++ vault/policies/config.tf
@@ -0,0 +1,39 @@
+#=============================#
+# AWS Provider Settings       #
+#=============================#
+provider "aws" {
+  alias                   = "main_region"
+  region                  = var.region
+  profile                 = var.profile
+  shared_credentials_file = "~/.aws/${var.project}/config"
+}
+
+provider "aws" {
+  alias                   = "secondary_region"
+  region                  = var.region_secondary
+  profile                 = var.profile
+  shared_credentials_file = "~/.aws/${var.project}/config"
+}
+
+provider "vault" {
+  address     = var.vault_address
+
+  /*
+  Vault token that will be used by Terraform to authenticate.
+  admin token from https://portal.cloud.hashicorp.com/.
+  */
+  token       = var.vault_token
+}
+
+terraform {
+  required_version = ">= 0.14.2"
+
+  required_providers {
+    aws = "~> 3.0"
+    vault = "~> 2.18.0"
+  }
+
+  backend "s3" {
+    key = "vault/policies/terraform.tfstate"
+  }
+}
