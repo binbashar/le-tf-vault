cbe60253019e2caa5705760ebd9767e0ba98ed41
Diego OJeda (BinBash) <38356409+diego-ojeda-binbash@users.noreply.github.com>
2021-05-28 19:03:12 -0300

    Update build file and terraform library (#9)

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ from leverage import path
 from _lib import terraform
 
 @task()
-def checkdir():
+def _checkdir():
     '''Check if current directory can execute certain tasks'''
     if path.get_working_path() == path.get_root_path():
         print("This task cannot be run from root path")
@@ -17,39 +17,50 @@ def checkdir():
         print("This task cannot be run from account path")
         sys.exit(0)
 
-@task(checkdir)
+@task(_checkdir)
 def init(*args):
-    '''Initialize Terraform in this layer.'''
+    '''
+    Initialize Terraform in this layer. For instance:
+                                        > leverage init
+                                        > leverage init["-reconfigure"]
+    '''
     terraform.init(list(args))
     terraform.change_terraform_dir_ownership()
 
-@task(checkdir)
+@task(_checkdir)
 def plan(*args):
     '''Generate a Terraform execution plan for this layer.'''
     terraform.plan(list(args))
 
-@task(checkdir)
+@task(_checkdir)
 def apply(*args):
-    '''Build or change the Terraform infrastructre in this layer.'''
+    '''
+    Build or change the Terraform infrastructre in this layer. For instance:
+                                        > leverage apply
+                                        > leverage apply["-auto-approve"]
+    '''
     terraform.apply(list(args))
-    terraform.change_terraform_dir_ownership()
 
-@task(checkdir)
+@task(_checkdir)
 def output(*args):
-    '''Show all terraform output variables of this layer.'''
-    terraform.output()
+    '''
+    Show all terraform output variables of this layer. For instance:
+                                        > leverage output
+                                        > leverage output["-json"]
+    '''
+    terraform.output(list(args))
 
-@task(checkdir)
+@task(_checkdir)
 def destroy(*args):
     '''Destroy terraform infrastructure in this layer.'''
     terraform.destroy(list(args))
 
 @task()
-def shell():
+def shell(*args):
     '''Open a shell into the Terraform container in this layer.'''
-    terraform.shell()
+    terraform.shell(list(args))
 
-@task(checkdir)
+@task(_checkdir)
 def version():
     '''Print terraform version.'''
     terraform.version()
@@ -64,17 +75,33 @@ def format_check():
     '''Check if Terraform files do not meet the canonical format.'''
     terraform.format_check()
 
-@task(checkdir)
+@task(_checkdir)
 def decrypt():
     '''Decrypt secrets.tf file.'''
     os.system("ansible-vault decrypt --output secrets.dec.tf secrets.enc")
 
-@task(checkdir)
+@task(_checkdir)
 def encrypt():
     '''Encrypt secrets.dec.tf file.'''
     os.system("ansible-vault encrypt --output secrets.enc secrets.dec.tf && rm -rf secrets.dec.tf")
 
-@task(checkdir)
+@task(_checkdir)
+def state():
+    '''Perform Terraform state operations.'''
+    print('''
+===============================================================================
+- IMPORTANT:
+===============================================================================
+- Leverage CLI does not yet support Terraform state operations. However this
+- task provides a small helper to make it a bit easier.
+- This task will present you with a shell that is ready to run Terraform state
+- commands such as `terraform state list` and more.
+===============================================================================
+''')
+    terraform.state()
+
+@task(_checkdir)
 def validate_layout():
     '''Validate the layout convention of this Terraform layer.'''
     return os.system("../../@bin/scripts/validate-terraform-layout.sh")
+
